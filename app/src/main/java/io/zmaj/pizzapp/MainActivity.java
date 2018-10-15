8caf5e8af5759dcd66c408098ab65aec40d875f3
darkzmaj <Malapatkica21#>
2018-10-15 21:21:16 +0200

    Initial commit

---
+++ app/src/main/java/io/zmaj/pizzapp/MainActivity.java
@@ -0,0 +1,31 @@
+package io.zmaj.pizzapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a();
+            }
+        });
+    }
+
+    public void a() {
+        Intent in = new Intent(this, BasicActivity.class);
+        startActivity(in);
+    }
+}
